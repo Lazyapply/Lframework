295c6588fcec6d5bde10b6b495d7214b07f42d87
Lazyapply <lazyapply@gmail.com>
2014-11-21 13:44:22 +0100

    update fixed

--- app/modules/usuarios/usuarios.model.php
+++ app/modules/usuarios/usuarios.model.php
@@ -138,8 +138,8 @@
 
 					$q .= " WHERE idUsuario='".$uId."'";
 
-				echo $q;
-				//$this->setQuery($q);
+				//echo $q;
+				$this->setQuery($q);
 				$this->execute_single_query();
 				unset($_POST);
 		}
